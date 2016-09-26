a444d90421d0ceb67582311e62b6dac29899b1fd
Darren Fang <darrenfang@qq.com>
2016-09-26 16:05:39 +0800

    修复dll选择的bug
    根据 PlatformTarget, TargetFrameworkVersion
    和Prefer32Bit这3个变量判断目标平台是x86还是x64

--- src/tools/install.ps1
+++ src/tools/install.ps1
@@ -1,42 +1,36 @@
 param($installPath, $toolsPath, $package, $project)
 
-$list = @("ManagedONS")    
+$list = @("ManagedONS")
 
-# Full assembly name is required
+# Need to load MSBuild assembly if it's not loaded yet.
 Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
 
-$projectCollection = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection
- 
-# There is no indexer on ICollection<T> and we cannot call
-# Enumerable.First<T> because Powershell does not support it easily and
-# we do not want to end up MethodInfo.MakeGenericMethod.
-$allProjects = $projectCollection.GetLoadedProjects($project.Object.Project.FullName).GetEnumerator(); 
+# Grab the loaded MSBuild project for the project
+$currentProject = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
 
-if($allProjects.MoveNext())
+foreach($reference in $currentProject.GetItems('Reference'))
 {
-    $currentProject = $allProjects.Current
-    foreach($reference in $currentProject.GetItems('Reference'))
-    {
-        # Get Include attribute of Reference Element
-        $include = $reference.Xml.Include
-    
-        foreach($item in $list){
+    # Get Include attribute of Reference Element
+    $include = $reference.Xml.Include
+
+    foreach($item in $list){
+        
+        # If Include match
+        if ($include -match ".*$item.*$"){
+            # Get HintPath of Reference Element
+            $hintPath = $reference.GetMetadataValue("HintPath")
             
-            # If Include match
-            if ($include -match ".*$item.*$"){
-                # Get HintPath of Reference Element
-                $hintPath = $reference.GetMetadataValue("HintPath")
-                
-                # Update HintPath
-                $newHintPath = $hintPath -replace '(.*\\)(x64)(\\.*\.dll)$', '$1$(PlatformTarget)$3'
-                $newHintPath = $newHintPath -replace '(.*\\)(x86)(\\.*\.dll)$', '$1$(PlatformTarget)$3'
-                $newHintPath = $newHintPath -replace '(.*\\)(AnyCPU)(\\.*\.dll)$', '$1$(PlatformTarget)$3'
-                
-                Write-Host "Update HintPath to $newHintPath"
-                $newMetadataValue = $reference.SetMetadataValue("HintPath", $newHintPath);
-            }
+            # Update HintPath
+            $newHintPath = $hintPath -replace '(.*\\)(x64)(\\.*\.dll)$', '$1$(BuildPlatform)$3'
+            $newHintPath = $newHintPath -replace '(.*\\)(x86)(\\.*\.dll)$', '$1$(BuildPlatform)$3'
+            $newHintPath = $newHintPath -replace '(.*\\)(AnyCPU)(\\.*\.dll)$', '$1$(BuildPlatform)$3'
+            
+            Write-Host "Update HintPath to $newHintPath"
+            $newMetadataValue = $reference.SetMetadataValue("HintPath", $newHintPath);
         }
     }
-    
-    $currentProject.Save()
 }
+
+
+# Save the project
+$project.Save()
